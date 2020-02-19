5925c265fbc170995ce9c01f593e79d44170306e
wertons <paubestardsatorra@gmail.com>
2020-02-19 20:22:20 +0100

    a

--- Check_insert.sql
+++ Check_insert.sql
@@ -22,15 +22,25 @@ left join staff	on person.DNI = staff.dni;
 /*2 Consultar cuantos puntos/goles/etc. ha conseguido en su carrera:*/
 select sportman.dni, team_name as team, person.first_name, sum(score) from sportman inner join team on sportman.team_code=team.team_code left join person on sportman.dni = person.dni group by first_name;
 
+
+select * from Player_Plays_Game;
 /*3 Consultar cuantos partidos ha ganado en su vida */
 select p.first_name,p.last_name,count(*) as 'Games won' from sportman 
 left join Player_Plays_Game pg on pg.dni = sportman.dni 
 left join person p on p.dni = sportman.dni 
+left join game on game.date_match = pg.date_match
 where sportman.team_code = 
 case
 	when sportman.team_code like pg.id_local_team then pg.id_local_team
     else pg.id_guest_team
-end;
+end
+and
+case 
+	when sportman.team_code like pg.id_local_team then game.local_result
+    else game.guest_result
+end 
+= "Winner"
+;
 
 /*4 Consultar cuantos jugadores hay por deporte */
 select discipline, count(case when person.gender='Male' then 'Male' ELSE NULL END) as male, count(case when person.gender='Female' then 'Female' ELSE NULL END) as female from sportman left join person on person.dni = sportman.dni  group by discipline;
@@ -47,7 +57,9 @@ Select team_name, discipline from team order by team_name asc;
 /*8 Listado de equipos ordenados por numero de fans */
 select team_name, count(ft.dni) as 'Number of fans' from team 
 left join Fan_Supports_Team ft on ft.team_code = team.team_code
-left join fan f on f.dni = ft.dni;
+left join fan f on f.dni = ft.dni
+group by team_name
+order by count(ft.dni) desc;
 
 /*9 Ultimo encuentro en el que lesiono un jugador */
 select first_name,injury_date from injuries
@@ -75,7 +87,7 @@ order by person.country
 ;
 
 /*13 Listar jugadores que juegan en ligas fuera de su pais */
-select first_name, last_name from Person
+select first_name, last_name , Person.country  as "Home country",  t.country as "Tournament Country" from Person
 left join Sportman s on s.dni = person.dni
 left join Player_Plays_Game pg on pg.dni = s.dni
 left join Tournament t on t.id_tournament = pg.id_tournament
@@ -89,7 +101,6 @@ case
 	when s.discipline is not null then s.job
     else 'Sportman'
 end as 'Type'
-
 ,first_name,last_name,
 case 
 	when s.discipline is not null then s.discipline
@@ -128,3 +139,4 @@ left join Fan_Attends_Game fa on fa.date_match = g.date_match AND
                                  fa.id_guest_team = g.id_guest_team
 order by count(dni) desc
 ;
+select count(*) from Game ;
