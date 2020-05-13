abf9b5f7353e31dfb6086b69e4e001812bf7aa58
wertons <paubestardsatorra@gmail.com>
2020-05-13 19:34:42 +0200

    trigger

--- Sport_triggers.sql
+++ Sport_triggers.sql
@@ -15,7 +15,7 @@ delimiter //
 CREATE TRIGGER update_Number_Players AFTER INSERT
 ON sportman FOR EACH ROW
 BEGIN
-	UPDATE team SET number_players = number_players + 1 
+	UPDATE team,sportman SET number_players = number_players + 1 
     WHERE sportman.team_code = team.team_code;
 END //
 
