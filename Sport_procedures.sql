fc2751423d9e1bc735f17f26748e5103ee31b4e7
wertons <paubestardsatorra@gmail.com>
2020-05-13 18:06:29 +0200

    qol changes

--- Sport_procedures.sql
+++ Sport_procedures.sql
@@ -3,7 +3,6 @@
 /*Query ID from maximum score player-Date from next game -- Fernando*/
 
 DELIMITER //
-
 CREATE PROCEDURE MaxPuntuation(Sport int)
 BEGIN
 declare NameSport int;
@@ -11,9 +10,7 @@ set NameSport = Sport;
 
 Select max(score) from sportman where id_discipline = NameSport;
 END //
-
-Delimiter ;
-call MaxPuntuation(1);
+DELIMITER ;
 /*Update team data-sportsman data -- Pablo*/
 
 /*Remove team-sportsman -- Pau*/
@@ -22,23 +19,17 @@ CREATE PROCEDURE RemoveSportman(DNI varchar(9))
 BEGIN
 declare _DNI varchar(9);
 set _DNI   = DNI;
-
 delete from sportman where dni = _DNI ;
-
 END//
 DELIMITER ;
 
 DELIMITER //
-
 CREATE PROCEDURE RemoveTeam(team_code int)
 BEGIN
 declare _team_code int;
 set _team_code   = team_code;
-
 delete from team where team_code = _team_code ;
-
 END//
-
 DELIMITER ;
 
 /*add procedure -> ???? -> profit*/
