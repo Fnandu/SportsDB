ed177411b9a1a740d603f9a7fc71481b4e7b2a36
wertons <paubestardsatorra@gmail.com>
2020-05-13 17:23:04 +0200

    added requirements

---
+++ Sport_procedures.sql
@@ -0,0 +1,9 @@
+/*Insert Teams-Sportsmans-Games -- Raul*/
+
+/*Query ID from maximum score player-Date from next game -- Fernando*/
+
+/*Update team data-sportsman data -- Pablo*/
+
+/*Remove team-sportsman -- Pau*/
+
+/*add procedure -> ???? -> profit*/
