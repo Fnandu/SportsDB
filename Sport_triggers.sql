42a4c776e2056b3478d8ced03458881ce1fae153
Pedro <pedroribeirobatista1998@gmail.com>
2020-05-13 18:50:27 +0200

    Segundo trigger

--- Sport_triggers.sql
+++ Sport_triggers.sql
@@ -1,9 +1,21 @@
 delimiter //
-CREATE TRIGGER ejemplo AFTER INSERT
+CREATE TRIGGER update_Number_Players AFTER INSERT
 ON sportman FOR EACH ROW
 BEGIN
 	UPDATE team SET number_players = number_players + 1 
     WHERE sportman.team_code = team.team_code;
-END
+END \\
+
+delimiter ;
+
+
+delimiter //
+
+CREATE TRIGGER New_contract AFTER UPDATE
+ON staff_works_for_team FOR EACH ROW
+BEGIN
+	UPDATE staff_works_for_team SET contract_expiry = null
+	WHERE staff_works_for_team.NEW = staff_works_for_team.NEW;
+END \\
 
 delimiter ;
